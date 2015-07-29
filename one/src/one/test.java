49a4f6ef59a5b6ced4eecb7fb3d1a60345a5bad2
Panos Parthenhs <pparthenhs@gmail.com>
2015-07-29 20:11:08 +0300

    change2

--- one/src/one/test.java
+++ one/src/one/test.java
@@ -2,12 +2,15 @@ package one;
 
 public class test {
 
-	public static void main (){
+	public static void main (String[] args){
 		System.out.print("ok");
 		
 		int x=1;
 		System.out.print("x="+x);
 		
+		int y=1;
+		int z=x+y;
+		System.out.print("z="+z);
 		
 	}
 
